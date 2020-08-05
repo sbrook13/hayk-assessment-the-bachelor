cc5dadddcd993354541887a018af9e590880b19e
sbrook13 <sbrook13@gmail.com>
2020-08-05 17:13:18 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,44 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+
+def all_seasons(data)
+  data.values.flatten
+end  
+
+def get_first_name_of_season_winner data, season
+  winning_contestant = data[season].find do |contestant|
+    contestant["status"] == "Winner"
+  end
+  winning_contestant["name"].split.first
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_hash = all_seasons(data).find do |contestant|
+    contestant["occupation"] == occupation
+  end
+  contestant_hash["name"]
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+  all_seasons(data).reduce(0) do |sum, contestant|
+    if contestant["hometown"] == hometown
+      sum + 1
+    else 
+      sum 
+    end
+  end
+end  
 
 def get_occupation(data, hometown)
-  # code here
+  all_seasons(data).find do |contestant|
+    contestant["hometown"] == hometown
+  end["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_sum = data[season].reduce(0) do |sum, contestant|
+    sum + contestant["age"].to_i
+  end
+  (age_sum / data[season].length.to_f).round()  
 end
